53f9437c7d5c61a22d69f29ade8290d7086f5b68
mahvec <victoropone@gmail.com>
2022-09-28 17:15:13 +0100

    commit 3.1
    updated the console.log at app.listen

--- app.js
+++ app.js
@@ -65,7 +65,7 @@ app.post('/failure', function (req, res) {
 })
 
 app.listen(process.env.PORT || 4000, function() {
-    console.log('Server is running on port 4000');
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
 
 //API key
