525d8464cc4cc755164e5d6b92b7e1686d77a188
mahvec <victoropone@gmail.com>
2022-09-28 17:19:40 +0100

    Update app.js

--- app.js
+++ app.js
@@ -65,7 +65,8 @@ app.post('/failure', function (req, res) {
 })
 
 app.listen(process.env.PORT || 4000, function() {
-    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+    
+console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
 
 //API key
